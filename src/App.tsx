5edbd7e33a4303dbd2253b9f0c711ca8c5f62827
cnkamanyi <cnkamanyi@hotmail.com>
2020-02-23 17:31:19 +0100

    Added everything necessary and this app works with redux saga. It can be downloaded and started now as is

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,44 @@
-import React from 'react';
+import * as React from 'react';
 import logo from './tr85logo.png';
 import './App.css';
 
-const App = () => {
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { Store } from 'redux';
+import { History } from 'history';
+import { ThemeProvider } from 'emotion-theming';//https://github.com/emotion-js/emotion/tree/master/packages/emotion-theming
+
+import Routes from './routes'
+import { ApplicationState } from './store'
+import LayoutContainer from './containers/LayoutContainer'
+import * as themes from './styles/theme'
+
+// Any additional component props go here.
+interface MainProps {
+  store: Store<ApplicationState>
+  history: History
+}
+
+const App : React.FunctionComponent<MainProps> = ({store, history}) => {
+  return(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+    	    <LayoutContainer>
+            {({theme}) => (
+                <ThemeProvider theme={themes[theme]}>
+                  <Routes />
+                </ThemeProvider>
+
+              )}
+          </LayoutContainer>
+      </ConnectedRouter>
+
+    </Provider>    
+  );
+}
+
+
+/* const App = () => {
   return (
     <div className="App">
       <header className="App-header">
@@ -12,5 +48,5 @@ const App = () => {
     </div>
   );
 }
-
-export default App;
+*/
+export default App; 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createBrowserHistory } from 'history';
+
+import configureStore from './configureStore';
+import 'typeface-ibm-plex-sans';
+
+const history = createBrowserHistory();
+const initialState = window.INITIAL_REDUX_STATE;
+const store = configureStore(history, initialState);
+
+ReactDOM.render(<App store={store} history={history} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
